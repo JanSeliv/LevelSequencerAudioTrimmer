1126325f09f9d80426705b975eba834b2610f63d
JanSeliv <janseliw@gmail.com>
2024-08-23 20:42:53 -0400

    Moved `Trim Audio` function from python script (that required installed ffmpeg separately) to the cpp code with built-in ffmpeg library as third party, so user does not need to install anything manually

--- Python/audio_reimporter.py
+++ Python/audio_reimporter.py
@@ -1,51 +1,12 @@
 import os
 import sys
-import subprocess
 import unreal
-import imageio_ffmpeg
-
 
 def get_file_size_mb(path):
     """Returns the size of the file in MB."""
     size_in_bytes = os.path.getsize(path) if os.path.exists(path) else 0
     size_in_mb = size_in_bytes / (1024 * 1024)
     return size_in_mb
-
-
-def trim_audio(input_path, output_path, start_ms, end_ms):
-    """Trims the audio file based on the start and end times in milliseconds."""
-    prev_size_mb = get_file_size_mb(input_path)
-
-    try:
-        output_dir = os.path.dirname(output_path)
-        os.makedirs(output_dir, exist_ok=True)
-
-        ffmpeg_path = imageio_ffmpeg.get_ffmpeg_exe()
-
-        ffmpeg_command = [
-            ffmpeg_path,
-            '-i', input_path,
-            '-ss', f'{start_ms / 1000:.2f}',
-            '-to', f'{end_ms / 1000:.2f}',
-            '-c', 'copy',
-            output_path,
-            '-y'
-        ]
-
-        result = subprocess.run(ffmpeg_command, capture_output=True, text=True)
-        if result.returncode != 0:
-            unreal.log_warning(f"FFmpeg failed to trim audio. stderr: {result.stderr}")
-            return False
-
-        new_size_mb = get_file_size_mb(output_path)
-
-        unreal.log(f"Trimmed audio stats: Previous Size: {prev_size_mb:.2f} MB, New Size: {new_size_mb:.2f} MB")
-
-        return True
-    except Exception as e:
-        unreal.log_error(f"Error trimming audio: {str(e)}")
-        return False
-
 
 def process_audio(level_sequence_path):
     """Processes each audio section in the level sequence."""
@@ -75,12 +36,15 @@ def process_audio(level_sequence_path):
             unreal.log_warning(f"Failed to export {sound_wave.get_name()}. Skipping...")
             continue
 
+        # Calculate trim times
         start_time_ms, end_time_ms = unreal.AudioTrimmerUtilsLibrary.calculate_trim_times(level_sequence, audio_section)
-        unreal.log(
-            f"Calculated Trim Times for {sound_wave.get_name()}: Start = {start_time_ms} ms, End = {end_time_ms} ms")
+        start_time_sec = start_time_ms / 1000.0
+        end_time_sec = end_time_ms / 1000.0
 
         trimmed_audio_path = export_path.replace(".wav", "_trimmed.wav")
-        if not trim_audio(export_path, trimmed_audio_path, start_time_ms, end_time_ms):
+
+        # Trim the audio using the C++ function
+        if not unreal.AudioTrimmerUtilsLibrary.trim_audio(export_path, trimmed_audio_path, start_time_sec, end_time_sec):
             unreal.log_warning(f"Trimming audio failed for {sound_wave.get_name()}. Skipping...")
             continue
 
@@ -97,7 +61,6 @@ def process_audio(level_sequence_path):
         unreal.AudioTrimmerUtilsLibrary.delete_temp_wav_file(trimmed_audio_path)
 
     unreal.log("Processing complete.")
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
--- Source/LevelSequencerAudioTrimmer/Private/LevelSequencerAudioTrimmer.cpp
+++ Source/LevelSequencerAudioTrimmer/Private/LevelSequencerAudioTrimmer.cpp
@@ -2,16 +2,16 @@
 
 #include "LevelSequencerAudioTrimmer.h"
 //---
+#include "LevelSequencerAudioTrimmerEdModule.h"
+//---
 #include "AudioTrimmerUtilsLibrary.h"
 #include "LevelSequence.h"
-#include "Interfaces/IPluginManager.h"
 #include "IPythonScriptPlugin.h"
 
 // Main entry point for the Level Sequencer Audio Trimmer plugin
 void FLevelSequencerAudioTrimmer::ProcessLevelSequence(const ULevelSequence& LevelSequence)
 {
-	FString CurrentPluginLocation = IPluginManager::Get().FindPlugin(TEXT("LevelSequencerAudioTrimmer"))->GetBaseDir();
-	CurrentPluginLocation = FPaths::ConvertRelativePathToFull(CurrentPluginLocation);
+	const FString& CurrentPluginLocation = FLevelSequencerAudioTrimmerEdModule::GetPluginPath();
 	const FString PythonScriptPath = FPaths::Combine(CurrentPluginLocation, TEXT("Python"), TEXT("audio_reimporter.py"));
 	const FString LevelSequencePath = LevelSequence.GetPathName();
 	const FString PythonCommand = FString::Printf(TEXT("import sys; sys.argv = ['%s', '%s']; exec(open('%s').read())"), *PythonScriptPath, *LevelSequencePath, *PythonScriptPath);
