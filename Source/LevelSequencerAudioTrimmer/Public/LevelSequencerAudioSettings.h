0078d950580aab84ebba83910cd5a1e69acbd24d
JanSeliv <janseliw@gmail.com>
2024-08-25 03:44:39 -0400

    Added plugin section in the Project Settings; Moved harcoded 200ms value to the config value property

---
+++ Source/LevelSequencerAudioTrimmer/Public/LevelSequencerAudioSettings.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "Engine/DeveloperSettings.h"
+//---
+#include "LevelSequencerAudioSettings.generated.h"
+
+/**
+ * Developer Settings for the Level Sequencer Audio Trimmer plugin.
+ * Is set up in 'Project Settings' -> "Plugins" -> "Level Sequencer Audio Trimmer".
+ */
+UCLASS(Config = LevelSequencerAudioTrimmer, DefaultConfig, DisplayName = "Level Sequencer Audio Trimmer")
+class LEVELSEQUENCERAUDIOTRIMMERED_API ULevelSequencerAudioSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	/** Returns Project Settings Data of the Level Sequencer Audio Trimmer plugin. */
+	static const FORCEINLINE ULevelSequencerAudioSettings& Get() { return *GetDefault<ThisClass>(); }
+
+	/** Gets the settings container name for the settings, either Project or Editor */
+	virtual FName GetContainerName() const override { return TEXT("Project"); }
+
+	/** Gets the category for the settings, some high level grouping like, Editor, Engine, Game...etc. */
+	virtual FName GetCategoryName() const override { return TEXT("Plugins"); }
+
+	/** Skip processing if the difference between total audio duration and section usage is less than this value. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Config, Category = "Audio Trimming", meta = (ClampMin = "0", UIMin = "0"))
+	int32 MinDifferenceMs;
+};
